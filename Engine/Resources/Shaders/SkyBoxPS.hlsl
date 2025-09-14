de7ae4ae46908e3f714fab75f599a04963fa34af
Olowerr <oliver.klingspor@hotmail.com>
2025-09-14 15:18:30 +0200

    clarified SkyBosPS

--- Engine/Resources/Shaders/SkyBoxPS.hlsl
+++ Engine/Resources/Shaders/SkyBoxPS.hlsl
@@ -2,14 +2,14 @@
 
 float3 getEnvironmentLight(float3 direction)
 {
-    const static float3 SKY_COLOUR = float3(102.f, 204.f, 255.f) / 255.f;
+    const static float3 SKY_BASE_COLOUR = float3(102.f, 204.f, 255.f) / 255.f;
     const static float3 GROUND_COLOUR = float3(164.f, 177.f, 178.f) / 255.f;
     
     const float dotty = dot(direction, float3(0.f, 1.f, 0.f));
-    float3 colour = lerp(float3(1.f, 1.f, 1.f), SKY_COLOUR, pow(max(dotty, 0.f), 0.6f));
-    
     const float transitionDotty = clamp((dotty + 0.005f) / 0.01f, 0.f, 1.f);
-    return lerp(GROUND_COLOUR, colour, transitionDotty);
+    
+    float3 skyColour = lerp(float3(1.f, 1.f, 1.f), SKY_BASE_COLOUR, pow(max(dotty, 0.f), 0.6f));
+    return lerp(GROUND_COLOUR, skyColour, transitionDotty);
 }
 
 float4 main(SkyBoxVSOutput vsOutput) : SV_TARGET
